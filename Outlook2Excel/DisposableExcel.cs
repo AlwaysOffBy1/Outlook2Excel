826269819d1ddf7843ca4ba03e85f5a38b613365
bg2508 <bg2508@WORKBENCH>
2025-07-18 08:21:31 -0400

    Working as intended

--- Outlook2Excel/DisposableExcel.cs
+++ Outlook2Excel/DisposableExcel.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 using Microsoft.Office.Interop.Excel;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Outlook2Excel
 {
